42eaf50531a6fa142bcd259af19361a728aa2743
Jon <jonpoischen@gmail.com>
2018-12-12 17:59:42 +0100

    changed back to no AsyncStorage

--- components/RegForm.js
+++ components/RegForm.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { StyleSheet, ScrollView, Text, TextInput, TouchableOpacity, Keyboard, Button, Image} from 'react-native';
+import { View, Dimensions, StyleSheet, ScrollView, Text, TextInput, TouchableOpacity, Keyboard, Button, Image } from 'react-native';
 import { ImagePicker, Permissions, Constants } from 'expo';
+import RadioGroup from 'react-native-radio-buttons-group';
 import firebase from 'firebase';
 
 
@@ -12,37 +13,75 @@ export default class RegForm extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            value: '',
             result: null,
             image: null,
             authenticating: false,
             email : '',
             password: '',
-            errorMessage : null
+            errorMessage : null,
+            disabled: true,
+            gender: '',
+            data: [
+                {
+                    label: 'Male',
+                    color: 'blue',
+                },
+                {
+                    label: 'Female',
+                    color: 'pink',
+                }
+            ]
         };
         this.onSubmit = this.onSubmit.bind(this);
+        this.checkLength = this.checkLength.bind(this);
+    }
+
+    componentDidMount() {
+        this.checker = setInterval(
+            () => this.checkLength(),
+            250
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.checker);
+    }
+
+    checkLength() {
+        if(this.state.email.length > 6 && this.state.password.length > 5 && this.state.bio
+           && this.state.firstName && this.state.lastName && this.state.age) {
+            this.setState({disabled:false});
+        } else {
+            this.setState({disabled:true});
+        }
     }
 
     onSubmit() {
         if (!this.state.firstName || !this.state.lastName || !this.state.age || !this.state.email || !this.state.password || !this.state.bio) {
             alert("Please fill out all fields.");
         } else {
+            console.ignoredYellowBox = ['Setting a timer'];
             let self = this;
+            if (this.state.data[0].selected) {
+                self.setState({gender: 'male'});
+            } else if (this.state.data[1].selected) {
+                self.setState({gender: 'female'});
+            }
             firebase.auth()
             .createUserWithEmailAndPassword(this.state.email, this.state.password)
             .then(function(data){
-                console.log("user uid: ", data.user.uid)
-                console.log("what is the state: ", self.state);
-                firebase.database().ref('/users/' + data.user.uid).set({
+                firebase.database().ref('/users/' + self.state.gender + '/' + data.user.uid).set({
                     firstName: self.state.firstName,
                     lastName: self.state.lastName,
                     age: self.state.age,
                     bio: self.state.bio,
                     pic: self.state.result.base64
                 })
+            .catch(error => {console.error(error);})
                 .then(() => {
                     this.props.navigation.navigate('EntranceScreen')
                 })
+                .catch(error => {console.error(error);})
             })
             .catch(error => this.setState({ errorMessage: error.message }))
         }
@@ -62,10 +101,16 @@ export default class RegForm extends React.Component {
         this.setState({ result });
     };
 
+    onPress = (data) => {
+        this.setState({ data });
+    };
+
     render() {
         let { image } = this.state;
+        let selectedButton = this.state.data.find(e => e.selected == true);
+        selectedButton = selectedButton ? selectedButton.value : this.state.data[0].label;
         return (
-            <ScrollView style={styles.regform}>
+            <ScrollView style={styles.container}>
                 <Text style={styles.header}>Register</Text>
                 <Text style={{ color: 'red' }}>
                     {this.state.errorMessage}
@@ -105,11 +150,16 @@ export default class RegForm extends React.Component {
                     style={styles.textinput}
                     placeholder="Last Name"
                     underlineColorAndroid={'transparent'}
-                    returnKeyType = {"next"}
-                    onSubmitEditing={() => {this.fifthTextInput.focus();}}
                     onChangeText={(lastName) => this.setState({lastName})}
                     isRequired={true}
                 />
+                <View>
+                    <Text>Select Gender</Text>
+                    <RadioGroup
+                        radioButtons={this.state.data}
+                        onPress={this.onPress}
+                    />
+                </View>
                 <TextInput
                     ref={(input) => {this.fifthTextInput = input;}}
                     style={styles.textinput}
@@ -132,25 +182,36 @@ export default class RegForm extends React.Component {
                     title="Upload Profile Pic"
                     onPress={this.useLibraryHandler}
                 />
-                {this.state && this.state.result &&
-                <Image source={{ uri: `data:image/gif;base64,${this.state.result.base64}` }} style={{ width: 200, height: 200 }} />}
-                <TouchableOpacity onPress={this.onSubmit} style={styles.button}>
-                <Text style={styles.btntext}>Sign Up</Text>
-                </TouchableOpacity>
+                {this.state && this.state.result && <Image
+                    source={{ uri: `data:image/gif;base64,${this.state.result.base64}` }}
+                    style={{ width: 200, height: 200 }}
+                />}
+                <View style={styles.button}>
+                    <Button
+                    title="Sign Up"
+                    onPress={this.onSubmit}
+                    disabled={this.state.disabled}
+                    />
+                </View>
             </ScrollView>
         );
     }
 }
 
+let deviceWidth = Dimensions.get('window').width;
+
 const styles = StyleSheet.create({
-    regform: {
-        alignSelf: 'stretch',
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignSelf: 'center',
     },
     header: {
+        alignSelf: 'center',
         fontSize: 24,
-        color: 'black',
         paddingBottom: 10,
         marginBottom: 40,
+        marginTop: 50,
         borderBottomColor: '#199187',
         borderBottomWidth: 1,
     },
@@ -158,19 +219,13 @@ const styles = StyleSheet.create({
         alignSelf: 'stretch',
         height: 40,
         marginBottom: 30,
-        color: 'black',
         borderBottomColor: '#f8f8f8',
         borderBottomWidth: 1,
     },
     button: {
-        alignSelf: 'stretch',
-        alignItems: 'center',
-        padding: 20,
-        backgroundColor: '#59cbbd',
-        marginTop: 30,
-    },
-    btntext: {
-        color: '#fff',
-        fontWeight: 'bold'
+        width: deviceWidth - 40,
+        marginBottom: 10,
+        paddingTop: 10,
+        flex: 1,
     },
 });
